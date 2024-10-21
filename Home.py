f19c5e7b4e9c5d1f8e5ba51039dbe1ae2c9404c9
Bryan89MOM <bryantan8989@gmail.com>
2024-10-21 11:35:05 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import hmac
 
 # Streamlit home page setup
 st.set_page_config(
@@ -8,6 +9,36 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+# Function to check password
+def check_password():
+    """Returns `True` if the user had the correct password."""
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+            
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+    
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+        
+    return False
+
+# Check password for this page
+if not check_password():
+    st.stop()  # Stop if password check fails
+
+
 # Create a column layout to place the logo beside the title text
 col1, col2 = st.columns([1, 5])
 
@@ -15,7 +46,7 @@ with col1:
     st.image("assets/hdb_logo.png", width=80)
 
 with col2:
-    st.title("HDB Resale Guru   ")
+    st.title("HDB Resale Guru")
 
 st.markdown(
     """
@@ -25,7 +56,7 @@ st.markdown(
 
     ## How to Navigate
     - **Use the sidebar** to access different sections:
-        - **Hello Chatbot**: Ask questions about HDB resale and get answers powered by OpenAI.
+        - **Chatbot**: Ask questions about HDB resale and get answers powered by OpenAI.
         - **Resale Market Analysis Tool**: Get insights on past transactions.
         - **About Us**: Project scope, objectives, data sources, and features.
         - **Methodology**: Comprehensive explanation of the data flows and implementation details
@@ -34,7 +65,6 @@ st.markdown(
     - Simply type your questions in the chatbot section to get quick answers.
     - Explore the market analysis to stay informed about the latest trends and data in the resale flat market.
     
-
     Enjoy exploring and getting the information you need!
     """
 )
